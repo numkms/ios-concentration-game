af3081f3027548edf957ad0739e468ccb2e33f3b
Vladimir Kurdyukov <numkms@gmail.com>
2019-04-13 10:23:52 +1000

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Владимир Курдюков on 13/04/2019.
+//  Copyright © 2019 Владимир Курдюков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
